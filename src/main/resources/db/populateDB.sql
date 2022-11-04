d309c5510bc63caf49022cd66c2047ef29233d24
drdima <drdima2@gmail.com>
2022-11-04 12:34:13 -0500

    add init db for users table

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,6 @@
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin'),
+       ('Guest', 'guest@gmail.com', 'guest');
